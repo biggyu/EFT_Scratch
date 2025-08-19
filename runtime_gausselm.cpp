d5b0226c22a3350e866f997e0ed0717452333b2b
Seung Gyu Lee <sl2486@ilab1.cs.rutgers.edu>
2025-08-19 15:08:46 -0400

    fix: linenum

--- runtime_gausselm.cpp
+++ runtime_gausselm.cpp
@@ -18,14 +18,15 @@ ShadowMemory* smem = new ShadowMemory();
     smem->dump_summary("ShadowMemory Summary");
 
 template<typename T>
-inline void SMemStore(T* ptr, ShadowMemory* smem) {
-    smem->on_store((void*)ptr, *(ptr), fp_op::STORE, __LINE__, nullptr, nullptr);
+inline void SMemStore(T* ptr, ShadowMemory* smem, size_t linenum) {
+    // smem->on_store((void*)ptr, *(ptr), fp_op::STORE, __LINE__, nullptr, nullptr);
+    smem->on_store((void*)ptr, *(ptr), fp_op::STORE, linenum);
 }
 
 template <typename T>
-inline T SMemLoad(T* ptr, ShadowMemory* smem) {
+inline T SMemLoad(T* ptr, ShadowMemory* smem, size_t linenum) {
   T v = *ptr;
-  smem->on_load((void*)ptr, (double)v, fp_op::LOAD, __LINE__);
+  smem->on_load((void*)ptr, (double)v, fp_op::LOAD, linenum);
   return v;
 }
 
@@ -60,10 +61,10 @@ int main() {
     for(int i = 0; i < dim; i++) {
         for(int j = 0; j < dim; j++) {
             // SMEM_STORE(&A[i][j]);
-            SMemStore(&A[i][j], smem);
+            SMemStore(&A[i][j], smem, __LINE__);
         }
         // SMEM_STORE(&x[i]);
-        SMemStore(&x[i], smem);
+        SMemStore(&x[i], smem, __LINE__);
     }
     for(int i = 0; i < dim; i++) {
         for(int j = 0; j < dim; j++) {
@@ -79,28 +80,28 @@ int main() {
             // float Aji, Aii;
             // SMEM_LOAD(&A[j][i], Aji);
             // SMEM_LOAD(&A[i][i], Aii);
-            float Aji = SMemLoad(&A[j][i], smem);
-            float Aii = SMemLoad(&A[i][i], smem);
+            float Aji = SMemLoad(&A[j][i], smem, __LINE__);
+            float Aii = SMemLoad(&A[i][i], smem, __LINE__);
             float ratio = Aji / Aii;
 
             for(int k = 0; k < dim; k++) {
                 // float Ajk, Aik;
                 // SMEM_LOAD(&A[j][k], Ajk);
                 // SMEM_LOAD(&A[i][k], Aik);
-                float Ajk = SMemLoad(&A[j][k], smem);
-                float Aik = SMemLoad(&A[i][k], smem);
+                float Ajk = SMemLoad(&A[j][k], smem, __LINE__);
+                float Aik = SMemLoad(&A[i][k], smem, __LINE__);
                 A[j][k] = Ajk - ratio * Aik;
                 // SMEM_STORE(&A[j][k]);
-                SMemStore(&A[j][k], smem);
+                SMemStore(&A[j][k], smem, __LINE__);
             }
             // float xj, xi;
             // SMEM_LOAD(&x[j], xj);
             // SMEM_LOAD(&x[i], xi);
-            float xj = SMemLoad(&x[j], smem);
-            float xi = SMemLoad(&x[i], smem);
+            float xj = SMemLoad(&x[j], smem, __LINE__);
+            float xi = SMemLoad(&x[i], smem, __LINE__);
             x[j] = xj - ratio * xi;
             // SMEM_STORE(&x[j]);
-            SMemStore(&x[j], smem);
+            SMemStore(&x[j], smem, __LINE__);
         }
     }
 
@@ -110,42 +111,42 @@ int main() {
             // float Aji, Aii;
             // SMEM_LOAD(&A[j - 1][i - 1], Aji);
             // SMEM_LOAD(&A[i - 1][i - 1], Aii);
-            float Aji = SMemLoad(&A[j - 1][i - 1], smem);
-            float Aii = SMemLoad(&A[i - 1][i - 1], smem);
+            float Aji = SMemLoad(&A[j - 1][i - 1], smem, __LINE__);
+            float Aii = SMemLoad(&A[i - 1][i - 1], smem, __LINE__);
             float ratio = Aji / Aii;
 
             for(int k = 0; k < dim; k++) {
                 // float Ajk, Aik;
                 // SMEM_LOAD(&A[j - 1][k], Ajk);
                 // SMEM_LOAD(&A[i - 1][k], Aik);
-                float Ajk = SMemLoad(&A[j - 1][k], smem);
-                float Aik = SMemLoad(&A[i - 1][k], smem);
+                float Ajk = SMemLoad(&A[j - 1][k], smem, __LINE__);
+                float Aik = SMemLoad(&A[i - 1][k], smem, __LINE__);
                 A[j - 1][k] = Ajk - ratio * Aik;
                 // SMEM_STORE(&A[j - 1][k]);
-                SMemStore(&A[j - 1][k], smem);
+                SMemStore(&A[j - 1][k], smem, __LINE__);
             }
             // float xj, xi;
             // SMEM_LOAD(&x[j - 1], xj);
             // SMEM_LOAD(&x[i - 1], xi);
-            float xj = SMemLoad(&x[j - 1], smem);
-            float xi = SMemLoad(&x[i - 1], smem);
+            float xj = SMemLoad(&x[j - 1], smem, __LINE__);
+            float xi = SMemLoad(&x[i - 1], smem, __LINE__);
             x[j - 1] = xj - ratio * xi;
             // SMEM_STORE(&x[j - 1]);
-            SMemStore(&x[j - 1], smem);
+            SMemStore(&x[j - 1], smem, __LINE__);
         }
     }
     for(int i = 0; i < dim; i++) {
         // float xi, Aii;
         // SMEM_LOAD(&x[i], xi);
         // SMEM_LOAD(&A[i][i], Aii);
-        float xi = SMemLoad(&x[i], smem);
-        float Aii = SMemLoad(&A[i][i], smem);
+        float xi = SMemLoad(&x[i], smem, __LINE__);
+        float Aii = SMemLoad(&A[i][i], smem, __LINE__);
         x[i] = xi / Aii;
         // SMEM_STORE(&x[i]);
-        SMemStore(&x[i], smem);
+        SMemStore(&x[i], smem, __LINE__);
         A[i][i] = 1.0f;
         // SMEM_STORE(&A[i][i]);
-        SMemStore(&A[i][i], smem);
+        SMemStore(&A[i][i], smem, __LINE__);
     }
 
     for(int i = 0; i < dim; i++) {

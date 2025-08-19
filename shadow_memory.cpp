6dd5c5c00817a2e46cf3ce1627c8e83ea19dcb72
Seung Gyu Lee <sl2486@ilab1.cs.rutgers.edu>
2025-08-19 15:08:34 -0400

    fix: removed ts, operands

--- shadow_memory.cpp
+++ shadow_memory.cpp
@@ -1,18 +1,18 @@
 #include "shadow_memory.hpp"
 
-ShadowMemory::ShadowMemory() {
-    ShadowMemory::gloabl_ts_ = 0;
-}
+// ShadowMemory::ShadowMemory() {
+//     ShadowMemory::gloabl_ts_ = 0;
+// }
 
-smem_entry& ShadowMemory::on_store(void* addr, double value, fp_op op = fp_op::STORE, size_t linenum = 0, smem_entry* lhs = nullptr, smem_entry* rhs = nullptr) {
+smem_entry& ShadowMemory::on_store(void* addr, double value, fp_op op = fp_op::STORE, size_t linenum = 0) {
     auto& e = table[reinterpret_cast<uintptr_t>(addr)];
     e.value = value;
     e.error = 0.0;
-    e.timestamp = ++gloabl_ts_;
+    // e.timestamp = ++gloabl_ts_;
     e.opcode = op;
     e.linenum = linenum;
-    e.lhs = lhs;
-    e.rhs = rhs;
+    // e.lhs = lhs;
+    // e.rhs = rhs;
     return e;
 }
 
@@ -24,27 +24,53 @@ smem_entry& ShadowMemory::on_load(void* addr, double program_value, fp_op op = f
         auto& e = table[reinterpret_cast<uintptr_t>(addr)];
         e.value = program_value;
         e.error = 0.0;
-        e.timestamp = ++gloabl_ts_;
+        // e.timestamp = ++gloabl_ts_;
         e.opcode = op;
         e.linenum = linenum;
-        e.lhs = nullptr;
-        e.rhs = nullptr;
+        // e.lhs = nullptr;
+        // e.rhs = nullptr;
         return e;
     }
 }
-
-// bool ShadowMemory::has(void* addr) const {
-//     return table.count(addr) != 0;
-// }
 
 void ShadowMemory::dump_summary(const char* header = "Shadow memory summary") const {
     printf("=== %s ===\n", header);
     printf("entries: %zu\n", table.size());
     size_t count = 0;
     for (auto& kv : table) {
-        // if (count++ >= 8) break; // print a few sample entries
         const auto& e = kv.second;
-        printf("  addr=0x%zx\tvalue=%.17g\terr=%.3e\tts=%zu\n",
-                    kv.first, e.value, e.error, e.timestamp);
+        printf("  addr=0x%zx\tvalue=%20.17g\terr=%.3e\tln=%zu\n",
+                    kv.first, e.value, e.error, e.linenum);
+        // printf("  addr=0x%zx\tvalue=%.17g\terr=%.3e\tts=%zu\n",
+        //             kv.first, e.value, e.error, e.timestamp);
     }
 }
+
+// smem_entry& ShadowMemory::on_store(void* addr, double value, fp_op op = fp_op::STORE, size_t linenum = 0, smem_entry* lhs = nullptr, smem_entry* rhs = nullptr) {
+//     auto& e = table[reinterpret_cast<uintptr_t>(addr)];
+//     e.value = value;
+//     e.error = 0.0;
+//     // e.timestamp = ++gloabl_ts_;
+//     e.opcode = op;
+//     e.linenum = linenum;
+//     e.lhs = lhs;
+//     e.rhs = rhs;
+//     return e;
+// }
+
+// smem_entry& ShadowMemory::on_load(void* addr, double program_value, fp_op op = fp_op::LOAD, size_t linenum = 0) {
+//     auto it = table.find(reinterpret_cast<uintptr_t>(addr));
+//     if (it != table.end() && it->second.value == program_value) {
+//         return it->second;
+//     } else {
+//         auto& e = table[reinterpret_cast<uintptr_t>(addr)];
+//         e.value = program_value;
+//         e.error = 0.0;
+//         // e.timestamp = ++gloabl_ts_;
+//         e.opcode = op;
+//         e.linenum = linenum;
+//         e.lhs = nullptr;
+//         e.rhs = nullptr;
+//         return e;
+//     }
+// }
--- shadow_memory.hpp
+++ shadow_memory.hpp
@@ -12,25 +12,24 @@ enum class fp_op : uint32_t { INIT, LOAD, STORE, ADD, SUB, MUL, DIV, UNKNOWN };
 struct smem_entry {
     double error;
     double value;
-    size_t timestamp;
+    // size_t timestamp;
     fp_op opcode;
     size_t linenum;
-    smem_entry* lhs;
-    smem_entry* rhs;
+    // smem_entry* lhs;
+    // smem_entry* rhs;
 };
 
 
 class ShadowMemory {
     public:
-        ShadowMemory();
-        smem_entry& on_store(void* addr, double value, fp_op op, size_t linenum, smem_entry* lhs, smem_entry* rhs);
+        // ShadowMemory();
+        // smem_entry& on_store(void* addr, double value, fp_op op, size_t linenum, smem_entry* lhs, smem_entry* rhs);
+        smem_entry& on_store(void* addr, double value, fp_op op, size_t linenum);
         smem_entry& on_load(void* addr, double program_value, fp_op op, size_t linenum);
-        // bool has(void* addr);
         void dump_summary(const char* header) const;
         
     private:
-        // static uintptr_t key(void* p) { return reinterpret_cast<uintptr_t>(p); }
         map<uintptr_t, smem_entry> table;
-        size_t gloabl_ts_ = 0;
+        // size_t gloabl_ts_ = 0;
 };  
 #endif
